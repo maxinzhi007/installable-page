5cecf93a8be0779322264278266080eb1744f008
maxinzhi007 <maxinzhi007@gmail.com>
2022-08-18 16:31:43 +0630

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,7 @@ var derredprompt;
 
 window.addEventListener("beforeinstallprompt", (e) => {
     e.preventDefault();
-    derredprompt = e;
-})
-
-const button = document.querySelector('button');
-button.addEventListener("click", async() => {
-    let nice = derredprompt;
-    await nice.prompt();
-    derredprompt = null;
+    setTimeout(() =>{
+        e.prompt();   
+    }, 2000)
 });
