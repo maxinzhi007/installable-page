84de3dccbd9f90c439712b237766d712f02e1cf7
maxinzhi007 <maxinzhi007@gmail.com>
2022-08-18 15:59:31 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 if("serviceWorker" in navigator) {
     navigator.serviceWorker.register('./sw.js');
 }
+
+let derredprompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    derredprompt = e;
+})
+
+const button = document.querySelement('button');
+button.addEventListener("click", async() => {
+    derredprompt.prompt();
+    derredprompt = null;
+});
